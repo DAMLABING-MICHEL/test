88c6af0a99db2e07889ad1831f096be978c6d867
Damlabing Michel <sylviadamlabing@gmail.com>
2022-05-19 12:01:06 +0100

    retrait du dossier api du suivi

--- api/admin/bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-try{
-    $connect = new PDO("mysql:host=localhost;dbname=upload_file","root","");
-}
-catch(PDOException $e){
-    die("erreur de connexion a la base de donnees:".$e->getMessage());
-}
